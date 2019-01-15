1229ec8d4d033e283ec0be489750febd8af53014
Jerry You <mygoapp.aiur0820@gmail.com>
2019-01-15 10:32:59 +0800

    add Bool+ & Int+

---
+++ SwiftExtensions/Source/Bool+.swift
@@ -0,0 +1,16 @@
+//
+//  Bool+.swift
+//  SwiftExtensions
+//
+//  Created by 游鴻斌 on 2019/1/15.
+//  Copyright © 2019 JerryYou. All rights reserved.
+//
+
+import Foundation
+
+extension Bool {
+  ///Convert it into Int
+  var intValue: Int {
+    return self ? 1 : 0
+  }
+}
---
+++ SwiftExtensions/Source/Int+.swift
@@ -0,0 +1,16 @@
+//
+//  Int+.swift
+//  SwiftExtensions
+//
+//  Created by 游鴻斌 on 2019/1/15.
+//  Copyright © 2019 JerryYou. All rights reserved.
+//
+
+import Foundation
+
+extension Int {
+  ///Convert it into Bool
+  var boolValue: Bool {
+    return self != 0
+  }
+}
