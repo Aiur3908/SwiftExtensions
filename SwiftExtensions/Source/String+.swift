ded08fdabe35f9e0b12696d4b04f1581f70781f0
Jerry You <mygoapp.aiur0820@gmail.com>
2019-01-16 14:04:53 +0800

    add String+

---
+++ SwiftExtensions/Source/String+.swift
@@ -0,0 +1,17 @@
+//
+//  String+.swift
+//  SwiftExtensions
+//
+//  Created by 游鴻斌 on 2019/1/16.
+//  Copyright © 2019 JerryYou. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+
+  var isNotEmpty: Bool {
+    return !isEmpty
+  }
+
+}
